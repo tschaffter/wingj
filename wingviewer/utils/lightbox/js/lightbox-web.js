c911d9833f6e88b45aed3a3838dc90da8670b6dc
tschaffter <thomas.schaffter@gmail.com>
2020-12-26 19:00:45 -0800

    Fix close button link

--- wingviewer/utils/lightbox/js/lightbox-web.js
+++ wingviewer/utils/lightbox/js/lightbox-web.js
@@ -46,8 +46,8 @@
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'utils/lightbox/images/loading.gif',
-    fileBottomNavCloseImage: 'utils/lightbox/images/closelabel.gif',
+    fileLoadingImage:        'http://localhost/utils/lightbox/images/loading.gif',
+    fileBottomNavCloseImage: 'http://localhost/utils/lightbox/images/closelabel.gif',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
